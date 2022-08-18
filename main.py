9e0bcb88e547077bc99192bae8a24f43929130c3
VadimChubar <chvagr@gmail.com>
2022-08-18 16:46:20 +0300

    Create main.py
    ДЗ 10

---
+++ main.py
@@ -0,0 +1,68 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value: str):
+        self.value = value
+
+    def __repr__(self):
+        return self.value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    pass
+
+
+class AddressBook(UserDict):
+
+    def add_contact(self, name: Name, phone: Phone = None):
+        contact = Record(name=name, phone=phone)
+        self.data[name.value] = contact
+
+    def add_record(self, record: "Record"):
+        self.data[record.name.value] = record
+
+    def find_by_name(self, name):
+        try:
+            return self.data[name]
+        except KeyError:
+            return None
+
+    def find_by_phone(self, phone: str):
+        for record in self.data.values():  # type: Record
+            if phone in [number.value for number in record.phones]:
+                return record
+        return None
+
+
+class Record:
+    def __init__(self, name: Name, phone: Phone = None):
+        self.name: Name = name
+        self.phones: list[Phone] = [phone] if phone is not None else []
+
+    def add_phone(self, phone_number: Phone):
+        self.phones.append(phone_number)
+
+    def change_phone(self, old_number: Phone, new_nuber: Phone):
+        try:
+            self.phones.remove(old_number)
+            self.phones.append(new_nuber)
+        except ValueError:
+            return f"{old_number} does not exists"
+
+    def delete_phone(self, phone: Phone):
+        try:
+            self.phones.remove(phone)
+        except ValueError:
+            return f"{phone} does not exists"
+
+
+book = AddressBook()
+new_record = Record(name=Name(value="user"))
+book.add_contact(name=Name(value="user_1"), phone=Phone(value="380000"))
+book.add_record(new_record)
+book.find_by_phone("380000").phones
